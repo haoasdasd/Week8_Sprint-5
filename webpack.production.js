1ba88aa5e2b228ebd5622ad444b859d8ab2e4fc5
anhhaole <154716906+haoasdasd@users.noreply.github.com>
2025-11-26 22:13:09 +0700

    Update webpack.production.js

--- webpack.production.js
+++ webpack.production.js
@@ -13,6 +13,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].bundle.js',
         clean: true,
+        publicPath: './' ,
         publicPath: config.publicPath
     },
     resolve: {
